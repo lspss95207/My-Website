8f10c338aad8c93ce0fb64cb0dc7b7141dfa3f49
Samuel Huang <lspss95207@gmail.com>
2020-05-12 11:03:18 +0800

    hw3 ball reflect

--- hw3/hw3.js
+++ hw3/hw3.js
@@ -1,3 +1,11 @@
+const height = 600;
+const width = 800;
+
+var gameArea = document.getElementById("gameArea");
+gameArea.style.height = height+"px";
+gameArea.style.width = width+"px";
+
+
 function Ball(){
     this.radius = randInt(5,20);
     this.velocity = {x: randInt(-5,5),y: randInt(-5,5)}
@@ -10,8 +18,16 @@ function Ball(){
     this.node.style.left = randInt(100,700) + "px";
 }
 Ball.prototype.move = function(){
-    this.node.style.top = parseInt(this.node.style.top)+this.velocity.y+"px";
-    this.node.style.left = parseInt(this.node.style.left)+this.velocity.x+"px";
+    let pos = {x:parseInt(this.node.style.left),y:parseInt(this.node.style.top)};
+    if(pos.x + 2*this.radius >= width || pos.x <= 0){
+        this.velocity.x *= -randInt(0.8,1.2);
+    }
+    if(pos.y + 2*this.radius >= height || pos.y <= 0){
+        this.velocity.y *= -randInt(0.8,1.2);    
+    }
+
+    this.node.style.top = Math.max(0,Math.min(height-2*this.radius,pos.y+this.velocity.y))+"px";
+    this.node.style.left = Math.max(0,Math.min(width-2*this.radius,pos.x+this.velocity.x))+"px";
 }
 
 
@@ -20,14 +36,16 @@ function randInt(start,end){
 }
 
 const N = 10;
-var container = document.getElementById("container");
+const fps = 60;
+
+
 ball_arr = [];
 for(let i = 0;i < N;i++){
     ball_arr[i] = new Ball();
-    container.appendChild(ball_arr[i].node);
+    gameArea.appendChild(ball_arr[i].node);
 }
 
-var id = setInterval(frame,200)
+var id = setInterval(frame,1000/fps)
 function frame(){
     ball_arr.forEach(ball => {
         ball.move();
@@ -35,4 +53,4 @@ function frame(){
 }
  
  
-setTimeout("clearInterval(id)",10000);
+setTimeout(()=>clearInterval(id),10000);
